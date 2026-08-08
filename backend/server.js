575873eee2004c66496acc025ff6345ccea39fe1
mohith <suseelamohith@gmail.com>
2026-08-08 17:50:27 +0530

    Fix routing syntax for Express 5 and confirm frontend path

--- backend/server.js
+++ backend/server.js
@@ -70,6 +70,6 @@ server.on('error', (err) => {
 app.use(express.static(path.join(__dirname, '../frontend')));
 
 // Catch-all route to send the HTML file
-app.get('*', (req, res) => {
+app.get('/(.*)', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
 });
